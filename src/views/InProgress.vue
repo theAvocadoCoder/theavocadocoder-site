1f5ff7aede7598182a64041e25602a60698ec2e2
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2021-12-26 12:39:05 +0100

    in-progress page added and modified

---
+++ src/views/InProgress.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="h-screen flex items-center justify-center">
+    <h1>
+      The developer is currently working on this page. How about you look at
+      <router-link to="/" @click="closeTab">some other pages</router-link>
+      for now?
+    </h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "InProgress",
+  methods: {
+    closeTab() {
+      window.top.close();
+    },
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+a.router-link-exact-active {
+  color: white;
+  text-decoration: underline;
+}
+</style>
